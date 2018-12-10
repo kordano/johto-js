5989ac7f36e14e5037164e4928eb475e40c77726
Konrad Kühne <konnykuehne@gmail.com>
2018-12-10 19:39:49 +0100

    import css in core component

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import 'spectre.css/dist/spectre.min.css';
+import 'spectre.css/dist/spectre-exp.min.css';
+import 'spectre.css/dist/spectre-icons.min.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
