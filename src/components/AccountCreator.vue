efa652927174eb700872ab8e3ac22ab03f5758b2
Konrad Kühne <konnykuehne@gmail.com>
2018-12-10 17:03:02 +0100

    add style and modal

--- src/components/AccountCreator.vue
+++ src/components/AccountCreator.vue
@@ -1,20 +1,52 @@
 <template>
   <div>
-    <h3>New Account</h3>
-    <div>
-      <input v-model.trim="firstname" placeholder="firstname">
-      <input v-model.trim="lastname" placeholder="lastname">
-      <select v-model.number="level" placeholder="level">
-        <option value="0">Other</option>
-        <option value="1">Extern</option>
-        <option value="2" selected>User</option>
-        <option value="3">Admin</option>
-      </select>
-      <input v-model.trim="email" placeholder="email">
-      <input v-model.trim="phone" placeholder="phone">
-      <input v-model.trim="occupation" placeholder="occupation">
+    <button v-on:click='toggleModal' class="btn btn-primary">New Account</button>
+    <div :class="modalStatus" id="new-account-modal">
+      <a href="#close" v-on:click='toggleModal' class="model-overlay" aria-label="Close"></a>
+      <div class="modal-container">
+        <div class="modal-header">
+          <a href="#close" v-on:click='toggleModal' class="btn btn-clear float-right" aria-label="Close"></a>
+          <div class="modal-title h5">New Account</div>
+        </div>
+      <div class="modal-body">
+        <div class="content">
+          <div class="form-group">
+            <label for="input-firstname" class="form-label">Firstname</label>
+            <input class="form-input" v-model.trim="firstname" id="input-firstname" placeholder="Firstname">
+          </div>
+          <div class="form-group">
+            <label for="input-lastname" class="form-label">Lastname</label>
+            <input class="form-input" id="input-lastname" v-model.trim="lastname" placeholder="Lastname">
+          </div>
+          <div class="form-group">
+            <label for="input-label" class="form-label">Level</label>
+            <select id="input-label" class="form-select" v-model.number="level" placeholder="Level">
+              <option value="0">Other</option>
+              <option value="1">Extern</option>
+              <option value="2" selected>User</option>
+              <option value="3">Admin</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label for="input-email" class="form-label">Email</label>
+            <input class="form-input" id="input-email" v-model.trim="email" placeholder="Email">
+          </div>
+          <div class="form-group">
+            <label for="input-phone" class="form-label">Phone</label>
+            <input class="form-input" id="input-phone" v-model.trim="phone" placeholder="Phone">
+          </div>
+          <div class="form-group">
+            <label for="input-occupation" class="form-label">Occupation</label>
+            <input class="form-input" id="input-occupation" v-model.trim="occupation" placeholder="Occupation">
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+      <button class="btn btn-primary" v-on:click='createAccount()'>Create</button>
+      <button class="btn btn-link" v-on:click='toggleModal'>Cancel</button>
+      </div>
+      </div>
     </div>
-    <button v-on:click='createAccount()'>ADD</button>
   </div>
 </template>
 
@@ -32,12 +64,21 @@ function isMissing(s: string): void {
 
 @Component
 export default class AccountCreator extends Vue {
+  modalStatus = 'modal';
   firstname = '';
   lastname = '';
   level = 0;
   email = '';
   phone = '';
   occupation = '';
+
+  toggleModal () {
+    if (this.modalStatus === 'modal active') {
+      this.modalStatus = 'modal';
+    } else {
+      this.modalStatus = 'modal active';
+    }
+  }
 
   createAccount () {
       if (this.firstname.length === 0  || this.firstname == null) {
@@ -66,6 +107,7 @@ export default class AccountCreator extends Vue {
         this.email = '';
         this.phone = '';
         this.occupation = '';
+        this.toggleModal();
       }
     }
 }
